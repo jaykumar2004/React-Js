046b8e31f69d5a6dfcc2bc60423e86a1343b3765
Jay Kumar jangid <106320129+jaykumar2004@users.noreply.github.com>
2024-09-26 19:46:12 +0530

    changes using vite

--- 001_vite_react_project/src/App.jsx
+++ 001_vite_react_project/src/App.jsx
@@ -1,9 +1,13 @@
-
+import Chai from "./chai"
 
 function App() {
 
   return (
-    <h1>starting react with vite</h1>
+    <>
+    <Chai/>
+    <h1>chai aur react</h1>
+    <p>test para</p>
+    </>
   )
 }
 
