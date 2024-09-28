0653d33d5b12d97d95e0e50f039139b7ab7b1ccb
Jay Kumar jangid <106320129+jaykumar2004@users.noreply.github.com>
2024-09-29 00:24:47 +0530

    creating own react element

--- 001_vite_react_project/src/App.jsx
+++ 001_vite_react_project/src/App.jsx
@@ -1,11 +1,12 @@
 import Chai from "./chai"
 
 function App() {
+  const userName = "chai Aur code"
 
   return (
     <>
     <Chai/>
-    <h1>chai aur react</h1>
+    <h1>chai aur react {userName}</h1>  
     <p>test para</p>
     </>
   )
--- 001_vite_react_project/src/main.jsx
+++ 001_vite_react_project/src/main.jsx
@@ -1,9 +1,50 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+function MyApp(){
+  return(
+    <div>
+      <h1>custom App | Jay Kumar</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {    //syntax is not right because we made it not react
+//   type : 'a',
+//   props : {    //props is a object
+//       href: "https://google.com",
+//       target : '_blank'
+//   },
+//   children : 'Click me to visit google'
+// }
+
+
+
+
+
+const anotherELement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "chai aur react"
+//we are using evaluated expression so we cannot write if or else because object has it's own syntax
+const reactElement = React.createElement(  //using react
+  //parameters are:
+  'a', //any tag
+  {href : 'https://google.com', target : '_blank'}, //object
+  'click me to visit google', //direct tag
+  //after this tree we can inject variables:
+  anotherUser,
+  anotherELement
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <ReactElement/>
+  // MyApp() //we can also write it as function
+  reactElement
+
+  // <App/>
+
 )
