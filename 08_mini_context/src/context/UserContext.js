bc26ef0ca79027338b9b26c7d8a31c5dd04a08f2
Jay Kumar jangid <106320129+jaykumar2004@users.noreply.github.com>
2024-10-13 01:45:38 +0530

    remaining
    remaining

---
+++ 08_mini_context/src/context/UserContext.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
+
+//to make user context:
+/*{ <userContext>
+    <Login />
+    <Card>
+        <Data />
+    </Card>
+</userContext> } */
+//with context we need to make provider
